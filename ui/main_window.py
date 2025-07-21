2abb1e58e87021111b3d8940a1815968bc2e31f8
nicolattarulo <71137853+nicolattarulo@users.noreply.github.com>
2025-07-21 19:28:23 -0400

    Added image stats tabs as well as some error handling to take in non siril images. Needs refining but feature works generally

--- ui/main_window.py
+++ ui/main_window.py
@@ -12,7 +12,7 @@ from core.project import Project
 from core.image_processing import process_fits_binaries
 from ui.sidebar.image_controls import ImageControlsCell
 from ui.sidebar.metadata_cell import MetadataCell
-from core.metadata import source_detect
+from core.metadata import source_detect, getBrightest, getnumStars
 
 
 
@@ -37,6 +37,16 @@ class MainWindow:
         self.starsToDetect = tk.IntVar(value = 1)
         self.brightest = None
         self.numStars = None
+
+        #Initialize image header for image stats and the desired info
+        self.fitsHeader = None
+        self.fLength = None
+        self.subTime = None
+        self.integrationTime = None
+        self.gain = None
+        self.objectRA = None
+        self.objectDec = None
+        self.pixelScale = None
 
         self.root.geometry("1200x800")
         self.root.minsize(1000, 600)  # Set minimum width and height
@@ -168,9 +178,30 @@ class MainWindow:
         # I believe this should just check if there's currently an image on the canvas
         if self.pil_image is not None:
             self.update_image_processing()
-            self.pil_image = source_detect(self.pil_image, self.current_project.get_fits_data('R'), self.starsToDetect.get()) # Pass in canvas image and one fits channel            
+            self.pil_image = source_detect(self.pil_image, self.current_project.get_fits_data('R'), self.starsToDetect.get()) # Pass in canvas image and one fits channel  
+            self.fitsHeader = self.current_project.get_fits_Header('R')
+            print(self.fitsHeader)
+            self.assignInfo()
+            self.metadata_cell.updateLabels(self.fitsHeader['FOCALLEN'], self.fitsHeader["EXPTIME"], self.integrationTime, self.gain, self.fitsHeader['OBJCTRA'], self.fitsHeader['OBJCTDEC'], self.pixelScale, getnumStars(), getBrightest())          
             self.image_viewer.load_image(self.pil_image)
             self.current_project.save_image(self.pil_image)
+
+    def assignInfo(self):
+        if 'LIVETIME' in self.fitsHeader:
+            self.integrationTime = {self.fitsHeader['LIVETIME']}
+        else:
+            self.integrationTime = -1  
+
+        if 'ISOSPEED' in self.fitsHeader:
+            self.gain = {self.fitsHeader['ISOSPEED']}
+        else:
+            self.gain = -1
+
+        if 'CDELT2' in self.fitsHeader:
+            self.pixelScale = {self.fitsHeader['LIVETIME']}
+        else:
+            self.pixelScale = -1
+        
             
 
     def close_current_project(self):
--- ui/sidebar/metadata_cell.py
+++ ui/sidebar/metadata_cell.py
@@ -1,5 +1,6 @@
 # Class to represent the UI widgets for the metadata cell  
 import tkinter as tk
+from tkinter import ttk
 class MetadataCell(tk.Frame):
      def __init__(self, parent, starsToDetect, on_starID):
         super().__init__(parent)
@@ -27,3 +28,59 @@ class MetadataCell(tk.Frame):
             command= self.on_starID
         )
         starID_button.pack(pady=10)
+
+        
+          #tab to switch views
+        self.dataTabControl = ttk.Notebook(self)    
+        self.dataTabControl.pack(pady=5, fill='both')
+
+        self.captureTab = ttk.Frame(self.dataTabControl)
+        self.astroTab = ttk.Frame(self.dataTabControl)
+        self.dataTabControl.add(self.captureTab, text="Image Data")
+        self.dataTabControl.add(self.astroTab, text = "Astro Data")
+
+        
+        #labels for image info
+        #imaga data
+
+        self.focalLengthLabel = tk.Label(self.captureTab, text = "Telescope Focal length: N/A")
+        self.focalLengthLabel.grid(row = 1, column = 0, sticky = "ew", pady = (5, 0))
+
+        self.shutterSpeedLabel = tk.Label(self.captureTab, text = "Camera exposure time: N/A")
+        self.shutterSpeedLabel.grid(row = 2, column = 0, sticky = "ew", pady = (5, 0))
+
+        self.integrationTimeLabel = tk.Label(self.captureTab, text = "Total Image Exposure Time (minutes): N/A")
+        self.integrationTimeLabel.grid(row = 3, column = 0, sticky = "ew", pady = (5, 0))
+
+        self.ISOLabel = tk.Label(self.captureTab, text="Sensor gain: N/A")
+        self.ISOLabel.grid(row=4, column=0, sticky="ew", pady=(5, 0))
+
+        #star data
+        self.pixelScaleLabel = tk.Label(self.astroTab, text = "Image Pixel Scale: N/A")
+        self.pixelScaleLabel.grid(row = 1, column = 0, sticky = "ew", pady = (5, 0))
+
+        self.RALabel = tk.Label(self.astroTab, text = "Image Right Asension: N/A")
+        self.RALabel.grid(row = 2, column = 0, sticky = "ew", pady = (5, 0))
+
+        self.DecLabel = tk.Label(self.astroTab, text = "Image Declination: N/A")
+        self.DecLabel.grid(row = 3, column = 0, sticky = "ew", pady = (5, 0))
+
+        self.numStarsLabel = tk.Label(self.astroTab, text = "Number of stars found: N/A")
+        self.numStarsLabel.grid(row = 4, column = 0, sticky = "ew", pady = (5, 0))
+
+        self.brightestLabel = tk.Label(self.astroTab, text="Brightest Star Magnitude: N/A")
+        self.brightestLabel.grid(row=5, column=0, sticky="ew", pady=(5, 0))
+
+     def updateLabels(self, focalLength, shutterSpeed, integrationTime, ISO, imageRA, imageDec, pixelScale, numStars, brightest):
+        #Mess with the labels
+        self.focalLengthLabel["text"] = f"Telescope Focal Length: {focalLength}"
+        self.shutterSpeedLabel["text"]= f"Camera Exposure Time: {shutterSpeed}"
+        self.integrationTimeLabel["text"]= f"Total Image Exposure Time: {integrationTime/60}"
+        self.ISOLabel["text"] = f"Sensor gain: {ISO}"
+
+        self.RALabel["text"]= f"Image Right Ascension: {imageRA}" 
+        self.DecLabel["text"]= f"Image Declination: {imageDec}" 
+        self.pixelScaleLabel["text"] = f"Image Pixel Scale: {pixelScale*3600:.2f} Arcsecond / pixel"
+        self.brightestLabel["text"] = f"Brightest Star Magnitude: {brightest:.2f}"
+        self.numStarsLabel["text"]= f"Number of Stars Detected: {numStars}" 
+
